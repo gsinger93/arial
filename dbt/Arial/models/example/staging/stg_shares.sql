ff0fc883291b275ae9dfd5707e59231a25870331
George <161592294+GSinger64@users.noreply.github.com>
2025-07-09 10:17:27 +0100

    refactor: Clean up dbt project configuration and SQL query formatting

--- dbt/Arial/models/example/staging/stg_shares.sql
+++ dbt/Arial/models/example/staging/stg_shares.sql
@@ -1,12 +1,16 @@
-    SELECT
-        CAST(price_date AS DATE) AS share_date,
-        open AS open_price,
-        high AS high_price,
-        low AS low_price,
-        close AS close_price,
-        volume AS trading_volume,
-        symbol
-    FROM `gs-arial.financial_data_landing.stock_values`
-    WHERE symbol IS NOT NULL
-    qualify row_number() over (partition by price_date, symbol order by ingest_dateTime desc) =1
-    -- This line ensures we only keep the most recent record for each symbol on each date.
+SELECT 
+  CAST(price_date AS DATE) AS share_date, 
+  open AS open_price, 
+  high AS high_price, 
+  low AS low_price, 
+  close AS close_price, 
+  volume AS trading_volume, 
+  symbol 
+FROM 
+  `gs-arial.financial_data_landing.stock_values` 
+WHERE 
+  symbol IS NOT NULL 
+  qualify row_number() over (
+    partition by price_date, symbol 
+    order by ingest_dateTime desc
+  ) = 1 -- This line ensures we only keep the most recent record for each symbol on each date.
